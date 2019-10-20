058c55fd1cfb568dab5153e6776a5dc08bb09be2
AlexanderPro <illarionov.pro@gmail.com>
2019-10-20 22:26:19 +0300

    Added UIAutomation references and AutomationElementExtensions.

---
+++ WindowTextExtractor/Extensions/AutomationElementExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Automation;
+using System.Windows.Automation.Text;
+
+namespace WindowTextExtractor.Extensions
+{
+    public static class AutomationElementExtensions
+    {
+        public static string GetText(this AutomationElement element)
+        {
+            object patternObj;
+            if (element.TryGetCurrentPattern(ValuePattern.Pattern, out patternObj))
+            {
+                var valuePattern = (ValuePattern)patternObj;
+                return valuePattern.Current.Value;
+            }
+            else if (element.TryGetCurrentPattern(TextPattern.Pattern, out patternObj))
+            {
+                var textPattern = (TextPattern)patternObj;
+                return textPattern.DocumentRange.GetText(-1).TrimEnd('\r');
+            }
+            else
+            {
+                return element.Current.Name;
+            }
+        }
+    }
+}
