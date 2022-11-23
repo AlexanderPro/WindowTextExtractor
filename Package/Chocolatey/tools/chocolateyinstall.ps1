33b4b07cc178bed8209548fc377eb730e05af166
AlexanderPro <illarionov.pro@gmail.com>
2022-11-23 20:33:39 +0300

    Added chocolatey package v1.15.0

---
+++ Package/Chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'windowtextextractor'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/AlexanderPro/WindowTextExtractor/releases/download/v1.15.0/WindowTextExtractor_v1.15.0.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  destination   = $toolsDir
+  fileType      = 'zip'
+  url           = $url
+  softwareName  = 'WindowTextExtractor*'
+  checksum      = '454f477dd62a2092a2aa09e751bc6a70cdb847a25d060b7e57bf7b8a426ba3c6'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
